38b30b7ed30c27c58558f55348787578fbb91423
GunaS911 <gunaswaminathan2567@gmail.com>
2026-06-05 14:43:32 +0530

    not_gate

---
+++ 001_Basic_Gates/007_Not_Gate/not_tb.v
@@ -0,0 +1,16 @@
+`timescale 1ns/1ps
+module not_tb;
+reg a;
+wire y;
+not_gate uut(
+.a(a),
+.y(y)
+);
+initial begin
+$dumpfile("not.vcd");
+$dumpvars(0,not_tb);
+a=0;#10;
+a=1;#10;
+$finish;
+end
+endmodule
