826fe964d6b3a2a7ca8876006b84741ca004b646
GunaS911 <gunaswaminathan2567@gmail.com>
2026-08-22 01:42:57 +0530

    4 bit binary counter

---
+++ HDL_Bits_Practice/004_Circuits/002_Sequential_Logic/002_Counters/003_Decade_counter_again.v
@@ -0,0 +1,12 @@
+module top_module (
+    input clk,
+    input reset,        // Synchronous active-high reset
+    output [3:0] q);
+    always @(posedge clk) begin
+        if (reset || q==10)
+            q<=4'b001;
+        else
+            q<= q + 4'b0001;
+    end
+endmodule
+
