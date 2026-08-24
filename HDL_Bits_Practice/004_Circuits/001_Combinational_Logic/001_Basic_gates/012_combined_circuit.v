f1bfd765b1ed83177c12a977b9d41a0f987fd011
GunaS911 <gunaswaminathan2567@gmail.com>
2026-08-25 00:06:17 +0530

    Moved HDL_bits as new repository

--- HDL_Bits_Practice/004_Circuits/001_Combinational_Logic/001_Basic_gates/012_combined_circuit.v
+++
@@ -1,8 +0,0 @@
-module top_module (input x, input y, output z);
-wire z1,z2,z3,z4;
-    assign z1 = (x^y)&x;
-    assign z2 = ~(x^y);
-    assign z3 = z1 | z2;
-    assign z4 = z1 & z2;
-    assign z = z3^z4;
-endmodule
--- HDL_Bits_Practice/004_Circuits/002_Sequential_Logic/001_Latches_and_FlipFlops/014_JK_using_DFF.v
+++
@@ -1,9 +0,0 @@
-module top_module (
-    input clk,
-    input j,
-    input k,
-    output Q); 
-    always @(posedge clk)begin
-        Q <= ~Q&j | ~k&Q;
-    end
-endmodule
